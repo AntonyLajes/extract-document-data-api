a291cd859e3aeff6e1a505883231d87b0bb47e4b
Antony Lajes <tonymateuslajes@hotmail.com>
2024-05-15 19:18:41 -0300

    fix: added cors in post endpoint

--- src/main/kotlin/com/example/demo/DocumentController.kt
+++ src/main/kotlin/com/example/demo/DocumentController.kt
@@ -4,10 +4,7 @@ import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayInputStream
 
@@ -15,6 +12,7 @@ import java.io.ByteArrayInputStream
 class DocumentController {
 
     @PostMapping("/extract-content")
+    @CrossOrigin(origins = ["http://localhost:8080"])
     fun extractText(@RequestParam("document") file: MultipartFile): ResponseEntity<String> {
         return try {
             val document = PDDocument.load(ByteArrayInputStream(file.bytes))
